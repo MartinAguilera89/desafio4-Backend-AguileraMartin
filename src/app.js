06a191c34ddf81fcffc6f49ee6aba0c56f53f10e
Martin Aguilera <maaguilera040113@gmail.com>
2023-10-10 23:51:14 -0300

    borrar codigo comentado

--- src/app.js
+++ src/app.js
@@ -5,12 +5,11 @@ import http from 'http';
 import { Server } from 'socket.io';
 import fs from 'fs';
 import expressHandlebars from 'express-handlebars';
-import path from 'path';
 
 
 const app = express();
-const server = http.createServer(app); // Crea un servidor HTTP
-const io = new Server(server); // Crea un servidor de Socket.IO
+const server = http.createServer(app);
+const io = new Server(server);
 
 const port = 8080;
 
@@ -20,27 +19,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-// Configura Handlebars como motor de plantillas
 app.engine('handlebars', expressHandlebars());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  // Renderiza la vista home.handlebars
-  const products = JSON.parse(fs.readFileSync('./src/data/products.json', 'utf-8')); // Lee products.json
+  const products = JSON.parse(fs.readFileSync('./src/data/products.json', 'utf-8'));
   res.render('home', { products });
 });
 
 app.get('/realtimeproducts', (req, res) => {
-  // Renderiza la vista realtimeProducts.handlebars
-  const products = JSON.parse(fs.readFileSync('./src/data/products.json', 'utf-8')); // Lee products.json
+  const products = JSON.parse(fs.readFileSync('./src/data/products.json', 'utf-8'));
   res.render('realtimeProducts', { products });
 });
 
 io.on('connection', (socket) => {
   console.log('Cliente conectado');
-  const products = JSON.parse(fs.readFileSync('./src/data/products.json', 'utf-8')); // Lee products.json
-  // Emitir la lista de productos a través de WebSocket
+  const products = JSON.parse(fs.readFileSync('./src/data/products.json', 'utf-8'));
   io.emit('productList', products);
 });
 
